ab7e7c5b360eb533e7d803062b4e5e19d9db245e
hugoarmor <hugoarmoreira@hotmail.com>
2024-03-09 13:15:28 -0300

    add: routing

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.scss';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
