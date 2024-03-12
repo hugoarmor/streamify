d8a890345ab44f3546322a84ac77b290368ceaca
hugoarmor <hugoarmoreira@hotmail.com>
2024-03-12 10:40:25 -0300

    add: file-queries

---
+++ client/src/queries/files.ts
@@ -0,0 +1,45 @@
+import { StreamifyFiles } from "../layouts/root/files";
+import { Http } from "../services/http/http.service";
+
+export class FileQueries {
+  static async getAll() {
+    const http = new Http();
+
+    const result = await http.get<StreamifyFiles>("api/files");
+
+    if (result.error) throw result.error;
+
+    return result.data;
+  }
+
+  static async destroy(path: string) {
+    const http = new Http();
+
+    const result = await http.delete<string>(`api/files/${path}`);
+
+    if (result.error) throw result.error;
+
+    return result.data;
+  }
+
+  static async rename({
+    oldPath,
+    newPath,
+  }: {
+    oldPath: string;
+    newPath: string;
+  }) {
+    const http = new Http();
+
+    const result = await http.patch<string>(
+      `api/files/${oldPath}/rename`,
+      {
+        new_file_path: newPath,
+      },
+    );
+
+    if (result.error) throw result.error;
+
+    return result.data;
+  }
+}
