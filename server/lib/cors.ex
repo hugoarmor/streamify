9e635fb77e1e83d081dc2cd57a5b8a1d0301e614
hugoarmor <hugoarmoreira@hotmail.com>
2024-03-12 10:34:04 -0300

    update: cors policy for allowed_headers

--- server/lib/cors.ex
+++ server/lib/cors.ex
@@ -2,7 +2,8 @@ defmodule Api.CORS do
   use Corsica.Router,
     origins: "*",
     allow_credentials: true,
-    max_age: 600
+    max_age: 600,
+    allow_headers: ["accept", "content-type", "authorization"]
 
   resource("/public/*", origins: "*")
   resource("/*")
