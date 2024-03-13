18b7b896fbb9b9cc9faf874bb5a7c44d8650792c
hugoarmor <hugoarmoreira@hotmail.com>
2024-03-13 16:58:28 -0300

    add: dropzone component

---
+++ client/src/components/dropzone.tsx
@@ -0,0 +1,27 @@
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import "./dropzone.style.scss";
+
+interface Props {
+  onDrop: (acceptedFiles: File[]) => void;
+}
+
+function MyDropzone({ onDrop }: Props) {
+  const handleDrop = useCallback((acceptedFiles: File[]) => {
+    onDrop(acceptedFiles);
+  }, [onDrop]);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop: handleDrop });
+
+  return (
+    <div
+      {...getRootProps()}
+      className={`bg-stf-purple-700 dashed-border ${isDragActive ? "border-stf-white" : ""}`}
+    >
+      <input {...getInputProps()} />
+      <p className='text-sm'>Drag & drop files here, or click to select files</p>
+    </div>
+  );
+};
+
+export default MyDropzone;
