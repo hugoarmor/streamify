240f2311908f2ce1cfdf62659d7c3df85bc3d023
hugoarmor <hugoarmoreira@hotmail.com>
2024-03-17 10:39:30 -0300

    update: FilesUploadProgress styles

--- client/src/components/files-upload-progress.tsx
+++ client/src/components/files-upload-progress.tsx
@@ -6,12 +6,12 @@ type Props = {
 
 export function FilesUploadProgress({ files }: Props) {
   return (
-    <div className="flex flex-col w-72 bg-stf-purple-700 border border-stf-purple-600 rounded-lg px-6 py-2 fixed bottom-6 right-6">
+    <div className="flex flex-col bg-stf-purple-700 border border-stf-purple-600 rounded-lg px-6 py-2 fixed bottom-6 right-6">
       <p className="font-semibold mb-2">Files Upload</p>
       <div className="flex flex-col gap-1">
         {files.map((file, index) => (
           <div key={"file-upload-" + index} className="font-extralight flex items-center gap-2">
-            <div className="w-4">
+            <div className="w-4 shrink-0">
               <CircularProgressbar
                 strokeWidth={15}
                 value={file.progress}
