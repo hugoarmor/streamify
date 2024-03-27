243c19c8dd68c224b18920ddf90b3b502c9e0d24
hugoarmor <hugoarmoreira@hotmail.com>
2024-03-27 10:17:17 -0300

    update: multipart-form byte length

--- server/lib/streamify_server_web/endpoint.ex
+++ server/lib/streamify_server_web/endpoint.ex
@@ -39,7 +39,7 @@ defmodule StreamifyServerWeb.Endpoint do
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
   plug Plug.Parsers,
-    parsers: [:urlencoded, :multipart, :json],
+    parsers: [:urlencoded, {:multipart, length: 200_000_000}, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 
